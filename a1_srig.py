eb4fc1ab9226bad58ace3c3bc5626051c9c32edb
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-09-08 13:04:06 -0700

    Close to finishing a1, TODO: fix sort, convert hex string to int, sort, convert to hex string, then remove leading 0s

--- a1_srig.py
+++ a1_srig.py
@@ -1,7 +1,17 @@
 import sys
+import collections
 with open(sys.argv[1], 'r') as data:
 	dataset = data.readlines()
 dataset = [x.strip('\n') for x in dataset]
+dataset = [int(x,16) for x in dataset]
+dataset.sort()
+dataset = [
+duplicates = collections.Counter(dataset)
+for key, value in duplicates.items():
+	if value > 1:
+		print(key, value)
 
-for items in dataset:
-	print(items)
+#print(duplicates)
+
+#for items in dataset:
+#	print(items)
