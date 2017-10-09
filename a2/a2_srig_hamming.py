9e8396bd3a99224362ff17634203fd886a573ae4
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-10-09 12:08:21 -0700

    started a2

---
+++ a2/a2_srig_hamming.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+#for x in sys.argv[1:]:
+	
+
+string1 = sys.argv[1]
+string2 = sys.argv[2]
+bin1 = bin(int(string1,16))
+bin2 = bin(int(string2,16))
+assert len(bin1)==len(bin2)
+
+hammingDist = sum(x != y for x,y in zip(bin1,bin2)) 
+
+print(hammingDist)
+	
