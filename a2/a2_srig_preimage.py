b89e7161780a1b27bfd28789698cf49c82062939
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-10-10 15:22:07 -0700

    now 3.3 4a is done. hopefully.

--- a2/a2_srig_preimage.py
+++ a2/a2_srig_preimage.py
@@ -1,12 +1,12 @@
-import os, sys, hashlib, random, string, binascii
-from random import randint
+import os, sys, hashlib, random, string
+
 hash1 = sys.argv[1]
 hash1 = hash1[0:6]
 hash2 = ' '
 count = 0
 
 while(hash1 != hash2):
-	randomStr = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(6+(count//1000000)))
+	randomStr = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(7+(count//750000)))
 	count=count+1
 	hash2 = hashlib.sha256(randomStr.encode('ascii')).hexdigest()
 	hash2 = hash2[0:6]
@@ -14,4 +14,5 @@ while(hash1 != hash2):
 print(randomStr)
 #ayy found unn10357ls
 #onov3n
-#should be working, but sometimes takes a while? normal?
+#yb3dufg4i4bod
+#sometimes really fast sometimes slow. normal?
