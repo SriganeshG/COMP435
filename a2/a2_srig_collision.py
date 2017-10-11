075d017dfa9023360eec0da65383d4fd01ee5dae
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-10-10 17:43:42 -0700

    collision cleaned up.

--- a2/a2_srig_collision.py
+++ a2/a2_srig_collision.py
@@ -4,9 +4,6 @@ count = 0
 
 hashes = dict()
 while True:
-	#randomStr2 = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(7+(count//750000)))
-	#hash2 = hashlib.sha256(randomStr2.encode('ascii')).hexdigest()
-	#hash2 = hash2[0:6]
 	randomStr1 = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(7))
 	hash1 = hashlib.sha256(randomStr1.encode('ascii')).hexdigest()
 	hash1 = hash1[0:6]
--- a2/a2_srig_preimage.py
+++ a2/a2_srig_preimage.py
@@ -4,9 +4,10 @@ hash1 = sys.argv[1]
 hash1 = hash1[0:6]
 hash2 = ' '
 count = 0
+randomStr = ''.join(random.choice(string.digits) for i in range(6))
 
 while(hash1 != hash2):
-	randomStr = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(7+(count//750000)))
+	randomStr = randomStr + ''.join(random.choice(string.ascii_lowercase) for i in range(1+(count//750000)))
 	count=count+1
 	hash2 = hashlib.sha256(randomStr.encode('ascii')).hexdigest()
 	hash2 = hash2[0:6]
